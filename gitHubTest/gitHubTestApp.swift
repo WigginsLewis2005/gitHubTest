0d12e4227d3b115d10b9f379ae4d5348ea0ff9c2
Linda <lianwi68@gmail.com>
2024-05-17 11:15:03 -0400

    Initial Commit

---
+++ gitHubTest/gitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubTestApp.swift
+//  gitHubTest
+//
+//  Created by Linda A. Lewis on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct gitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
