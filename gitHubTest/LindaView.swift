0ed525fda00e1cd08d6e2bf605766acf567e4694
Linda <lianwi68@gmail.com>
2024-05-17 11:35:25 -0400

    Added Linda View

---
+++ gitHubTest/LindaView.swift
@@ -0,0 +1,18 @@
+//
+//  LindaView.swift
+//  gitHubTest
+//
+//  Created by Linda A. Lewis on 5/17/24.
+//
+
+import SwiftUI
+
+struct LindaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LindaView()
+}
